d3fc06154906c58903e6c20a78f43ac187bc8f82
Аня Лобанова <annushka9999@mail.ru>
2020-11-22 23:10:54 +0200

    Исправлена ошибка с булевыми переменными в read и изменения в task

--- readFromConsole.php
+++ readFromConsole.php
@@ -3,10 +3,10 @@ function readFromConsole($question){
 	echo $question.': ';
 	$input = trim(fgets(STDIN));
 	if($input === '') {
-		$input = null;
+		$input = NULL;
 	}
 	elseif($input === 'true' || $input ==='false'){
-		$input = true;
+		$input = (bool)$input;
 	}
 	elseif (is_numeric($input)){
 		$input = +$input;
--- task1.php
+++ task1.php
@@ -1,13 +1,13 @@
 <?php
 require_once "readFromConsole.php";
-echo "Программа суммирования чисел";
-echo "Для завершения программы нажмите 2 раза Enter \n";
+echo "Программа суммирования чисел".PHP_EOL;
+echo "Для завершения программы нажмите 2 раза Enter или введите не число".PHP_EOL;
 
 $result=0;
 
 do{
 	$input = readFromConsole("Введите число");
 	if(is_numeric($input)) $result+=$input;
-}while($input != null);
+}while(is_numeric($input));
 
 echo "Сумма $result";
